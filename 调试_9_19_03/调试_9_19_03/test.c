5168bbcef7823c90d0bb07592ea2c5d9b1343e8c
韩晓东 <2279784962@qq.com>
2022-09-19 20:54:56 +0800

    提交代码

---
+++ 调试_9_19_03/调试_9_19_03/test.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//int main()
+//{
+//	int i=0;
+//	for(i=0;i<100;i++)
+//	{
+//		printf("%d ",i);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    int i = 0;
+//    int arr[10] = {0};
+//    for(i=0; i<=12; i++)
+//   {
+//        arr[i] = 0;
+//        printf("hehe\n");
+//   }
+//    return 0;
+//}
+
+//断点  F9切换断点
+//F5  直接运行到断点处（执行逻辑上的下一个断点）
+//int main()
+//{
+//	int i=0;
+//	for(i=0;i<100;i++)
+//	{
+//		printf("%d ",i);
+//	}
+//	for(i=0;i<100;i++)
+//	{
+//		printf("%d ",10-i);
+//	}	
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10]={0};
+//	int i=0;
+//	int tmp=0;
+//	{
+//		printf("tmp=%d\n",tmp);
+//	}
+//	for(i=0;i<10;i++)
+//	{
+//		arr[i]=i;
+//	}
+//
+//	return 0;
+//}
+
+
+
+//void test2()
+//{
+//	printf("haha\n");
+//}
+//void test1()
+//{
+//	test2();
+//}
+//void test()
+//{
+//	test1();
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+
