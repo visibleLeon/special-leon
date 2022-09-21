905904928fa503c720b16a216cda010e965eb0f3
韩晓东 <2279784962@qq.com>
2022-09-21 21:10:30 +0800

    上传代码

---
+++ 数据储存_9_21_02/数据储存_9_21_02/test.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	char a=128;
+//	printf("%u\n",a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i=-20;
+//	unsigned int j=10;
+//	printf("%d\n",i+j);
+//	return 0;
+//}
+
+//int main()
+//{
+//	unsigned int i;
+//	for(i=9;i>=0;i--)
+//	{
+//		printf("%u\n",i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//    char a[1000];
+//    int i;
+//    for(i=0; i<1000; i++)
+//   {
+//        a[i] = -1-i;
+//   }
+//    printf("%d",strlen(a));
+//    return 0;
+//}
+
+//#include <stdio.h>
+//unsigned char i = 0;
+//int main()
+//{
+//    for(i = 0;i<=255;i++)
+//   {
+//        printf("hello world\n");
+//   }
+//    return 0;
+//}
+
+//int main()
+//{
+// int n = 9;
+// float *pFloat = (float *)&n;
+// printf("n的值为：%d\n",n);
+// printf("*pFloat的值为：%f\n",*pFloat);
+// *pFloat = 9.0;
+// printf("num的值为：%d\n",n);
+// printf("*pFloat的值为：%f\n",*pFloat);
+// return 0;
+//}
+
+
+int main()
+{
+	float f=5.5;
+
+	return 0;
+}
