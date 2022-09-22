4e0790b822e8575e352827d44b2ab277171b1c14
韩晓东 <2279784962@qq.com>
2022-09-22 19:14:59 +0800

    代码

---
+++ 指针详解_9_22_02/指针详解_9_22_02/test.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+
+//void Print(char* str)
+//{
+//	printf("%s\n",str);
+//}
+//
+//int main()
+//{ 
+//	void(*p)(char*)=Print;
+//	(*p)("hello bit");
+//	return 0;
+//}
+
+//  
+
+#include <stdio.h>
+int add(int a, int b)
+{
+           return a + b;
+}
+int sub(int a, int b)
+{
+           return a - b;
+}
+int mul(int a, int b)
+{
+           return a*b;
+}
+int div(int a, int b)
+{
+           return a / b;
+}
+int main()
+{
+	    int x, y;
+     int input = 1;
+     int ret = 0;
+     int(*p[5])(int x, int y) = { 0, add, sub, mul, div }; //转移表
+     while (input)
+     {
+          printf( "*************************\n" );
+          printf( " 1:add           2:sub \n" );
+          printf( " 3:mul           4:div \n" );
+          printf( "*************************\n" );
+          printf( "请选择：" );
+      scanf( "%d", &input);
+          if ((input <= 4 && input >= 1))
+         {
+          printf( "输入操作数：" );
+              scanf( "%d %d", &x, &y);
+              ret = p[input](x, y);
+         }
+          else
+               printf( "输入有误\n" );
+          printf( "ret = %d\n", ret);
+     }
+      return 0;
+}
